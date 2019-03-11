89eee43264a7158d712a08e1c6cc07112ff473be
DESKTOP-D2V1UJK\Danilshik <danilshik@yandex.ru>
2019-03-11 14:28:25 +0300

    Добавлены модели ответа от сервера

---
+++ app/src/main/java/com/example/itunesapp/result/Response.java
@@ -0,0 +1,22 @@
+package com.example.itunesapp.result;
+
+import java.util.List;
+
+public class Response {
+    private int resultCount;
+
+    private List<Result> results;
+
+    public void setResultCount(int resultCount){
+        this.resultCount = resultCount;
+    }
+    public int getResultCount(){
+        return this.resultCount;
+    }
+    public void setResults(List<Result> results){
+        this.results = results;
+    }
+    public List<Result> getResults(){
+        return results;
+    }
+}
